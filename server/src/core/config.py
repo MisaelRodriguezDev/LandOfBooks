4b60eab78a6c72d5283042cfcc2493245405f277
Misael_Rodríguez <misaelrdz963@gmail.com>
2025-05-30 20:53:42 -0600

    init: first commit

---
+++ server/src/core/config.py
@@ -0,0 +1,29 @@
+from decouple import config
+
+class Config:
+    __instance = None
+
+    PORT: int = config("PORT", cast=int)
+    SECRET_KEY: str = config("SECRET_KEY")
+    DB_URL: str = config("DB_URL")
+    CLIENT_URL: str = config("CLIENT_URL")
+
+    RECAPTCHA: str = config("RECAPTCHA")
+
+    SMTP_SERVER: str = config("SMTP_SERVER")
+    SMTP_PORT: int = config("SMTP_PORT")
+    EMAIL_USER: str = config("EMAIL_USER")
+    EMAIL_PASSWORD: str = config("EMAIL_PASSWORD")
+
+
+    def __new__(cls):
+        if Config.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+    
+    def __setattr__(self, key, value):
+        if hasattr(self, key):
+            raise AttributeError(f"El atributo '{key}' no puede ser modificado")
+        super().__setattr__(key, value)
+    
+CONFIG = Config()
